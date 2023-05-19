306f4f9944a0dc26b3d8f566cb57a34f3bf41438
Cristian <114029162+RCristianooo@users.noreply.github.com>
2023-05-19 18:03:01 -0400

    Create README.d

---
+++ README.d
@@ -0,0 +1,25 @@
+# Visual Chronicles Portfolio 
+Visual Chronicles is a Portfolio made for Zephyr Stonefield, a photographer from Florida. I created his website in order to assist him in showcasing some of his photography to some of his clients. It is also considered to be center for anyone interesting in his work as well as a way to get in touch with Zephyr.
+
+Link to project: https://zephyrstonefield.netlify.app
+
+![Preview Visual Chronicles Portfolio](https://cristianroblero.netlify.app/images/fulls/visual-chronicles-zephyr-stonefield.png)
+
+## How It's Made:
+
+**Tech used:** HTML, CSS, JavaScript
+
+Visual Chronicles is the name of his freelancing business. As the single owner of Visual Chronicles, he sees his business as a project of his own. Taking pictures wherever he goes, often what he views to be magnificent in his own eyes. With photography being the center of his freelancing, his portfolio should be showing the images he has taken and nothing else. Therefore, Zephyr had a simple design in mind. He wanted a simple monochromatic colorway of blue. After a few suggestions, we went foward with a blue, a darker shade of blue, and black. He then only wanted a single section on the right that included the name of business, his name, social links, and finally many of his images. According to his instructions, the images in that section needed to be "smaller and clickable as if they were thumbnails on YouTube". The premise of his portfolio should be his pictures, therefore, when entered into the site, a person is introduced to his first image as ultimate showcasing. There is also an option to X out the section to enter fullscreen to his picture. The arrows on the left and right can be clicked to navigate over to all his other images. Each one reveals a title and description of the image. With another simple design, viewers can truly enjoy the pictures of Zephyr Stonefield in fullscreen!
+
+## Optimizations
+
+Zephyr was very glad about the outcome of the product. He was thrilled enough to  
+
+## Lessons Learned:
+
+Lessons I've learn
+
+## Examples:
+Take a look at these couple examples that I have in my own portfolio:
+
+**Curl Coiffure Hair Salon:** https://github.com/RCristianooo/Curl-Coiffure-Hair-Salon
